5a3cc358bff60e60c1d5fe97d60cca27da8c3938
IvanLuketic2002 <luketicivan242@gmail.com>
2026-07-09 21:45:25 +0200

    Oporavak primarnog regiona nakon crash testa

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,3 +43,30 @@ module "ec2_secondary" {
   subnet_cidr   = "10.1.1.0/24"
   instance_type = "t3.micro"
 }
+# --- HEALTH CHECK ZA FRANKFURT ---
+resource "aws_route53_health_check" "primary" {
+  ip_address        = module.ec2_primary.public_ip
+  port              = 8000
+  type              = "HTTP"
+  resource_path     = "/"
+  failure_threshold = "3" # Ako 3 puta za redom ne odgovori, smatra se da je pao
+  request_interval  = "30"
+
+  tags = {
+    Name = "${var.environment}-health-check-primary"
+  }
+}
+
+# --- HEALTH CHECK ZA PARIZ ---
+resource "aws_route53_health_check" "secondary" {
+  ip_address        = module.ec2_secondary.public_ip
+  port              = 8000
+  type              = "HTTP"
+  resource_path     = "/"
+  failure_threshold = "3"
+  request_interval  = "30"
+
+  tags = {
+    Name = "${var.environment}-health-check-secondary"
+  }
+}
